49d1aeacb680a8a2a2482cf03e4770ef6e60463d
Hadi Rickit <rickithadi@gmail.com>
2023-02-21 22:10:05 +0800

    components done

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -1,4 +1,4 @@
-import React  from "react";
+import React from "react";
 
 import ss from "../images/ss.png";
 import WaitlistForm from "../partials/WaitlistForm";
@@ -10,10 +10,12 @@ function HeroHome() {
 
       {/* Hero content */}
       <div className="pt-20 pb-12 md:pt-20 md:pb-20 mx-auto ">
-        <div className="md:grid md:grid-cols-12 md:gap-6 mx-auto h-full md:h-[70vh]">
+        <div className="md:grid md:grid-cols-12  mx-auto h-full md:h-[70vh]">
           <div className=" text-center  bg-[#FFFBF9]  max-w-xl md:max-w-none md:w-full mx-auto md:col-span-6 lg:col-span-6 mb-8 md:mb-0 md:order-1 aos-init -m-4">
-            <div className="md:py-40 px-10 md:py-32 pt-16
-            ">
+            <div
+              className=" px-10 md:py-32 pt-16
+            "
+            >
               <h2
                 className="text-5xl
                 text-left text-[#5AA568] mb-8
@@ -21,21 +23,31 @@ function HeroHome() {
                 leading-snug
                 "
               >
-               Budding, the app for social gatherings
+                Budding, the app for social gatherings
               </h2>
               <p
                 className="text-left text-xl text-gray-600 mb-8"
                 data-aos="zoom-y-out"
                 data-aos-delay="150"
               >
-              You are always surrounded by people who are looking for company, just like you. But how do you find them? Download Budding and swipe on the events you are up for.
+                You are always surrounded by people who are looking for company,
+                just like you. But how do you find them? Download Budding and
+                swipe on the events you are up for.
               </p>
 
               <WaitlistForm />
             </div>
           </div>
-          <div class="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-6 lg:col-span-6 mb-8 md:mb-0 md:order-1 aos-init">
-            <img src={ss} className="mx-auto h-3/4 py-14 px-10" />
+          <div
+            class="
+            max-w-xl md:max-w-none md:w-full mx-auto md:col-span-6 lg:col-span-6 mb-8 md:mb-0 md:order-1 aos-init
+
+          "
+          >
+            <img src={ss} className="mx-auto h-3/4 px-10 md:h-[95vh]
+            py-24
+ flex items-center justify-center
+            " />
           </div>
         </div>
       </div>
