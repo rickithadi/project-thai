9b42d4ad942bd2e19648ef7cf52b143ee3ddaee5
Hadi Rickit <rickithadi@gmail.com>
2023-02-21 23:14:11 +0800

    done

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 function Footer() {
-  return <footer className="bg-[#FFFBF9] h-10"></footer>;
+  return (
+    <footer className="bg-[#FFFBF9] h-10 grid-cols-2">
+      <div>
+        <h2
+          className="
+        py-2
+        px-5
+        text-left
+                text-[#5AA568]  mb-8
+                font-bold
+                leading-snug
+                "
+        >
+          Get amongst it
+        </h2>
+      </div>
+      <div>
+        <h2
+          className="
+        py-1
+                text-right text-[#5AA568]  mb-8
+                font-bold
+                leading-snug
+                "
+        >
+          email
+        </h2>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
