3a447048fbc1f5d9cea856234f742f5713db3109
Hadi Rickit <rickithadi@gmail.com>
2023-02-21 21:29:58 +0800

    responsive padding

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -9,34 +9,33 @@ function HeroHome() {
       {/* Illustration behind hero content */}
 
       {/* Hero content */}
-      <div className="pt-20 pb-12 md:pt-20 md:pb-20 mx-auto">
-        <div className="md:grid md:grid-cols-12 md:gap-6 mx-auto">
+      <div className="pt-20 pb-12 md:pt-20 md:pb-20 mx-auto ">
+        <div className="md:grid md:grid-cols-12 md:gap-6 mx-auto h-full md:h-[70vh]">
           <div className=" text-center  bg-[#FFFBF9]  max-w-xl md:max-w-none md:w-full mx-auto md:col-span-6 lg:col-span-6 mb-8 md:mb-0 md:order-1 aos-init -m-4">
-            <div className="md:py-40 px-10 sm:py-30
-            sm:pt-30
+            <div className="md:py-40 px-10 md:py-32 pt-16
             ">
               <h2
                 className="text-5xl
                 text-left text-[#5AA568] mb-8
-                font-extrabold
+                font-bold
                 leading-snug
                 "
               >
-                The place for social gatherings
+               Budding, the app for social gatherings
               </h2>
               <p
-                className="text-xl text-gray-600 mb-8"
+                className="text-left text-xl text-gray-600 mb-8"
                 data-aos="zoom-y-out"
                 data-aos-delay="150"
               >
-              Some copy that works
+              You are always surrounded by people who are looking for company, just like you. But how do you find them? Download Budding and swipe on the events you are up for.
               </p>
 
               <WaitlistForm />
             </div>
           </div>
           <div class="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-6 lg:col-span-6 mb-8 md:mb-0 md:order-1 aos-init">
-            <img src={ss} className="mx-auto" />
+            <img src={ss} className="mx-auto h-3/4 py-14 px-10" />
           </div>
         </div>
       </div>
